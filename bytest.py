38e32a1f7e211a073e13141277d07ef1f26f337b
164052-collab <164052@wsk.ac.th>
2026-07-27 03:03:33 +0000

    bytest.py

---
+++ bytest.py
@@ -0,0 +1,11 @@
+# Get dynamic inputs from the user (No longer fixed numbers)
+width = float(input("Enter the width: "))
+length = float(input("Enter the length: "))
+
+# Calculate the area
+area = width * length
+
+# Display the result
+print("--------------------")
+print("The area is:", area)
+print("--------------------")
